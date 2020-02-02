4f9ecaf2ac6a506c41eb2a23caec619a66069f2b
Simon Crowe <info@simoncrowe.net>
2020-02-02 21:33:51 +0000

    Extended food-making logic.

---
+++ Assets/Scripts/Environment Behaviour/Blind Audo/ChutesInventory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System;
+
+public class ChutesInventory : MonoBehaviour
+{
+
+    public int celeryCount = 8;
+    public int rutabagaCount = 5;
+    public int cornflourCount = 10;
+    
+    void Start()
+    {
+        EventManager.StartListening("remove_celery_from_chute", RemoveCelery);
+        EventManager.StartListening("remove_rutabaga_from_chute", RemoveRutabaga);
+        EventManager.StartListening("remove_cornflour_from_chute", RemoveCornflour);
+    }
+
+    void RemoveCelery()
+    {
+        if (celeryCount > 0)
+        {
+            celeryCount--;
+        }
+        else
+        {
+            throw new InvalidOperationException("There are no celery sticks left to remove.");
+        }
+    }
+
+    void RemoveRutabaga()
+    {
+        if (rutabagaCount > 0)
+        {
+            rutabagaCount--;
+        }
+        else
+        {
+            throw new InvalidOperationException("There are mo rutabagas left to remove.");
+        }
+    }
+
+    void RemoveCornflour()
+    {
+        if (cornflourCount > 0)
+        {
+            cornflourCount--;
+        }
+        else
+        {
+            throw new InvalidOperationException("There is no cornflour left to remove.");
+        }
+    }
+}
---
+++ Assets/Scripts/Environment Behaviour/Blind Audo/SinkAudio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewMonoBehaviour : MonoBehaviour
+{
+    public AudioClip waterPourSound;
+
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+    }
+}
