cce3dfe46cd623fbd123cadaf4e1297f753d9e55
Simon Crowe <info@simoncrowe.net>
2020-02-22 23:41:47 +0000

    Added mechanism for switching to scene 1.

---
+++ Assets/Scripts/User Movement/UserExitScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UserExitScene : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Had" || other.gameObject.name == "pushingCollider")
+        { 
+            SceneManager.LoadScene(1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/User Movement/WheelchairLooker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelchairLooker : MonoBehaviour
+{
+    public GameObject wheelchair;
+    public FirstPersonController firstPersonController;
+
+    void Start()
+    {
+        EventManager.StartListening("had_look_at_wheelchair", LookAtWheelchair);
+    }
+
+    void LookAtWheelchair()
+    {
+        // Update MouseLook's internal rotation state
+        transform.LookAt(wheelchair.transform.position +  new Vector3(0, 0.5f, 0));
+        firstPersonController.m_MouseLook.Init(
+             firstPersonController.gameObject.transform,
+             transform
+        );
+    }
+
+}
